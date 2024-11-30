6d13b63550baf8e7b0ca1de0b4d44a1ab5e9ada5
jospin6 <jospinndagano1@gmail.com>
2024-11-30 12:00:02 +0200

    Add the bottom navbar

---
+++ lib/screens/signalerPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignalerPage extends StatefulWidget {
+  const SignalerPage({super.key});
+
+  @override
+  State<SignalerPage> createState() => _SignalerPageState();
+}
+
+class _SignalerPageState extends State<SignalerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
