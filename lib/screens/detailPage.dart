d5f4a6cb96f7d9da2474c4be26a797f4a5b2392e
jospin6 <jospinndagano1@gmail.com>
2024-11-30 15:36:11 +0200

    Add card to display infos

--- lib/screens/detailPage.dart
+++ lib/screens/detailPage.dart
@@ -8,23 +8,83 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(emergencyDetail['title']!),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.network(emergencyDetail['image']!),
-            const SizedBox(height: 20),
-            Text(
-              emergencyDetail['details']!,
-              style: const TextStyle(fontSize: 18),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(emergencyDetail['title']!),
         ),
-      ),
-    );
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Stack(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 300,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(emergencyDetail['image']!),
+                        fit: BoxFit.cover)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Icône de flèche arrière et titre
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.arrow_back_ios),
+                          onPressed: () {
+                            // Action pour le bouton de retour
+                            Navigator.pop(context);
+                          },
+                        ),
+                        Text(
+                          emergencyDetail['title']!,
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                    // Bouton ElevatedButton
+                    ElevatedButton(
+                      onPressed: () {
+                        // Action pour le bouton
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red
+                            .withOpacity(0.3), // Couleur rouge avec opacité
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(10)), // Radius de 10
+                        ),
+                      ),
+                      child: const Text(
+                        'Appeler',
+                        style: TextStyle(color: Colors.white), // Texte en blanc
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                  child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                        topRight: Radius.circular(50))),
+                child: Column(
+                  children: [
+                    Text(
+                      emergencyDetail['details']!,
+                      style: const TextStyle(fontSize: 18),
+                    ),
+
+                    // AUTRES INFORMATIONS
+                
+                  ],
+                ),
+              )),
+            ],
+          ),
+        ));
   }
 }
--- lib/screens/infosPage.dart
+++ lib/screens/infosPage.dart
@@ -10,6 +10,86 @@ class InfosPage extends StatefulWidget {
 class _InfosPageState extends State<InfosPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Card(
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:  [
+                // Titre de la Card
+                const Text(
+                  'Titre de la Card',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10), // Espacement
+
+                // Column pour lieu, ville, commune
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Lieu: ',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    Text(
+                      'Kadutu',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 5), // Espacement
+
+                    Text(
+                      'Ville: ',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    Text(
+                      'Goma',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 5), // Espacement
+
+                    Text(
+                      'Commune: ',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    Text(
+                      'Nyiragongo',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10), // Espacement
+
+                // Description
+                const Text(
+                  'Voici une description de la Card, qui peut contenir des informations supplémentaires.',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10), // Espacement
+
+                // Image
+                Container(
+                  width: double.infinity,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: const DecorationImage(
+                      image: NetworkImage(
+                          'https://via.placeholder.com/300'), // Remplacez par votre URL d'image
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
